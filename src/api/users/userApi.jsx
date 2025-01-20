d2d2a3585827e9ef19cfd1ca174b2a068fcfe48f
eduongcoder <truongthaiduong0808@gmail.com>
2025-01-20 22:19:21 +0700

    Fix port 8082 to 8083

--- src/api/users/userApi.jsx
+++ src/api/users/userApi.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://26.232.136.42:8082';
+const BASE_URL = 'http://26.232.136.42:8083';
 export const getubtoken = (token) => {
-    return axios.get(`${BASE_URL}/api/users/getubtoken?token=${token}`, /* { headers: { 'Authorization': `Bearer ${token}` } }*/);
+    return axios.post(`${BASE_URL}/api/users/getubtoken?token=${token}`, /* { headers: { 'Authorization': `Bearer ${token}` } }*/);
 }
 export const register = (user) => {
   return axios.post(`${BASE_URL}/api/users/register`, user);}
